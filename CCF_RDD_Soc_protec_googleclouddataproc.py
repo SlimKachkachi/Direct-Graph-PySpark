b434535c44d5ca09ef7ecd275783dd78a99b5df6
SlimK <slim.kachkachi@mailo.fr>
2022-10-16 10:22:54 +0200

    versions DF & RDD cluster Google Cloud Dataproc

---
+++ CCF_RDD_Soc_protec_googleclouddataproc.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import pyspark
+sc = pyspark.SparkContext()
+
+import os
+
+def reducer(x):
+    key=x[0]
+    values=x[1]
+    min=key
+    valuesList=[]
+    listoutput=[]
+    global newpaircounter
+    for i in values:
+        if i <min:
+            min=i
+        valuesList.append(i)
+    if min < key:
+        listoutput.append((key,min))
+        for j in valuesList:
+            if min != j:
+              listoutput.append((j,min))
+              newpaircounter.add(1)
+    return listoutput
+
+
+path = os.getcwd()
+rdd_init = sc.textFile("/user/ccf_project/soc-pokec-relationships.txt")
+rdd=rdd_init.filter(lambda x:"#" not in x).map(lambda x:x.split("\t")).map(lambda x:(int(x[0]),int(x[1])))
+
+t0=time.time()
+boucle = True
+
+
+while boucle == True:
+ 
+  newpaircounter=sc.accumulator(0)
+  
+  #map du ccf
+  Map_rdd=rdd.flatMap(lambda x:(x,(x[1],x[0])))
+  
+  #fonction reduce du CCF Iterate RDD fichier de chiffres
+  Reduce_rdd=Map_rdd.groupByKey().flatMap(lambda x:reducer(x))
+  Reduce_rdd.count() 
+  
+  ##suite #fonction Reduce de CFF Dedup sur base RDD liste de chiffres pour vérifier la suppression des doublons de tuples
+  rdd=Reduce_rdd.distinct().sortByKey()
+
+  print(newpaircounter.value)
+  if newpaircounter.value == 0:
+    print("arret")
+    boucle = False
+
+t1=time.time()
+print("c'est la fin; durée totale de :",round(t1-t0,3))
---
+++ CFF_DF_Soc_protec_googlecloud_dataproc.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import pyspark
+
+rom pyspark.sql import SparkSession 
+spark = SparkSession.builder \
+  .appName('Slim Essai Spark DataFrames') \
+  .getOrCreate()
+
+from pyspark.sql.functions import *
+from pyspark.sql.types import ArrayType,IntegerType,MapType
+
+
+@udf(ArrayType(ArrayType(IntegerType())))
+def DFreducerBis(key,value):
+    min=key
+    valuesList=[]
+    listoutput=[]
+#   global accu
+    for i in value:
+        if i <min: 
+            min=i
+        valuesList.append(i)
+    if min < key:
+        listoutput.append((0,key,min))
+        for j in valuesList:
+            if min != j:
+              listoutput.append((1,j,min))
+#             accu.add(1)
+#   print(listoutput)
+    return listoutput
+
+DF_Init=spark.read.csv("/user/ccf_project/soc-pokec-relationships.txt",sep="\t")
+
+DF=DF_Init.withColumn("_c0",DF_Init["_c0"].cast("int")).withColumnRenamed("_c0","key")\
+                     .withColumn("_c1",DF_Init["_c1"].cast("int")).withColumnRenamed("_c1","value")
+
+#Initialisation de la boucle
+boucle = True
+t0=time.time()
+
+while boucle == True:
+
+  #map du CFF
+  MapDF=DF.union(DF.select('value','key')).groupBy("key").agg(collect_list("value").alias("MapOutput"))
+
+  #fonction reduce du CCF Iterate DF fichier de chiffres
+  ReduceDF=MapDF.withColumn("ListnewPairs",DFreducerBis("key","MapOutput")).select(explode(col('ListnewPairs'))).select([col("col")[i] for i in range(3)])
+
+  compteur=ReduceDF.where("col[0]=1").count()
+  print(compteur)
+
+  ##suite #fonction Reduce de CFF Dedup sur base RDD liste de chiffres pour vérifier la suppression des doublons de tuples
+  DF=ReduceDF.withColumnRenamed("col[1]","key").withColumnRenamed("col[2]","value").select("key","value").distinct()
+
+  if compteur == 0:
+    print("arret")
+    boucle = False
+
+t1=time.time()
+#le round de python interfère l'opérateur round de pspark sql (qui s'applique à des colonnes)
+print("c'est la fin; durée totale de :",t1-t0)
